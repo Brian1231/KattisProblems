ba5fef96f00f935ea3fc66c3618295df15e4f869
Brian1231 <brianoleary1995@gmail.com>
2017-05-09 13:37:20 +0100

    More 4thought

--- src/fourthought/Main.java
+++ src/fourthought/Main.java
@@ -15,11 +15,11 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		int N = s.nextInt();
 		int[] cases = new int[N];
 		char[] operators = {'+', '-', '*','/'};
-		
+
 		for(int i=0;i<N;i++){
 			cases[i] = s.nextInt();
 		}
@@ -27,17 +27,80 @@ public class Main {
 
 		printArray(cases);
 		System.out.println();
+		int ans = -1000;
 		
 		//Loop through cases
 		for(int i=0;i<N;i++){
+			System.out.println("\nCase: " + cases[i]);
 			
 			//Loop through operators
 			for(int j=0;j<4;j++){
-				
+				for(int k=0;k<4;k++){
+					for(int l=0;l<4;l++){
+						ans = calc(operators[j], operators[k], operators[l]);
+						System.out.println(ans);
+						if(ans == cases[i]){
+							System.out.println("4 " + operators[j] + " 4 " + operators[k] + " 4 " + operators[l] + " 4");
+							break;
+						}
+						if(ans == cases[i]) break;
+					}
+					if(ans == cases[i]) break;
+				}
+				if(ans == cases[i]) break;
 			}
+			if(ans != cases[i])
+				System.out.println("Impossible");
 		}
 	}
-	
+
+	private static int calc(char j, char k, char l){
+		//Loop through j
+		if(j == '+'){
+			return 4 + calc('0', k, l);
+		}
+		else if(j == '-'){
+			return 4 - calc('0', k, l);
+		}
+		else if(j == '*'){
+			return 4 * calc('0', k, l);
+		}
+		else if(j == '/'){
+			return 4 / calc('0', k, l);
+		}
+		else if(j == '0'){
+			//Loop through k
+			if(k == '+'){
+				return 4 + calc('0', '0', l);
+			}
+			else if(k == '-'){
+				return 4 - calc('0', '0', l);
+			}
+			else if(k == '*'){
+				return 4 * calc('0', '0', l);
+			}
+			else if(k == '/'){
+				return 4 / calc('0', '0', l);
+			}
+			else if(k == '0'){
+				//Loop through l
+				if(l == '+'){
+					return 4 + calc('0', '0', '0');
+				}
+				else if(l == '-'){
+					return 4 - calc('0', '0', '0'); //0
+				}
+				else if(l == '*'){
+					return 4 * calc('0', '0', '0');
+				}
+				else if(l == '/'){
+					return 4 / calc('0', '0', '0');
+				}
+			}
+		}
+		return 4;
+	}
+
 	private static void printArray(int[] array){
 		System.out.print("{");
 		for(int i=0;i<array.length;i++){
